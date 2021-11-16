5edcb552c2ae67dcb38c11d84a06404a1bd138a0
fogleins <fogleins@hotmail.hu>
2021-11-16 14:47:53 +0100

    Initial commit

---
+++ export/export_all_in_one.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# ha a python script nem fut: pip3 install termcolor
+
+# sima tobbszintes album eseten lehet, hogy elfogadhatoan mukodik, de olyan esetben, amikor az alalbumok
+# nem a teljes albummal egyszerre kerulnek exportra, nagy valoszinuseggel nem jo
+# a permissionok valtozasat mindig ellenorizd az export utan
+# read -r -p "A script hasznalata csak egyszintes album eseten javasolt. Biztosan folytatod? [i/n] " response
+# if [[ "$response" =~ ^([iI])$ ]] ; then
+#     : # no op
+# else
+#     echo "exiting"
+#     exit 1;
+# fi
+
+
+export_start=`date +%s`
+drive_name="teamdrive-2021"
+album_path=""
+
+# flagek kezelese
+while getopts 'd:a:' flag; do
+  case "${flag}" in
+    d) drive_name="${OPTARG}" ;;
+    a) album_path="${OPTARG}" ;;
+    *) echo "Ismeretlen flag. Hasznalat: -d drive_neve: az rclone.conf-ban beallitott driveok kozul az, amelyikrol exportalni szeretnel. -a album_neve a driveon belul az album eleresi utja. Pl: teamdrive-2021:/20211014_levelup eseten: -d teamdrive-2021 -a 20211014_levelup"
+       exit 1 ;;
+  esac
+done
+
+if [ "$album_path" == "" ]; then
+    echo "Add meg az album driveon beluli eleresi utjat."
+    exit 1
+fi
+
+check_json() {
+    json_path="./${album_path}/info.json"
+    head ${json_path}
+    read -r -p "json rendben? (i/n) " json_response
+    if [[ "$json_response" =~ ^([iI])$ ]] ; then
+        : # no op
+    else # ha a json hibas, megnyitjuk szerkesztesre
+        nano ${json_path}
+    fi
+}
+
+update_permissions() {
+    # fotok es a teljes mappa jogosultsagainak frissitese
+    # ${album_path:0:4} az evet adja vissza (albumnev elso negy karaktere)
+    # a valtozok utan szereplo ket vesszo (,,) a string valtozo kisbetusiteset szolgalja,
+    # ugyanis a python script minden albumnevet kisbetusit a hugo miatt
+    album_path_filesystem="/srv/spotweb/web_images/${album_path:0:4}/${album_path,,}"
+    chmod -R 775 ${album_path_filesystem}
+    chgrp -R users ${album_path_filesystem}
+
+    # md jogosultsagainak modositasa
+    md_path_filesystem="/srv/spotweb/content/photo/${album_path:0:4}/${album_path,,}.md"
+    chmod 775 ${md_path_filesystem}
+    chgrp users ${md_path_filesystem}
+
+    # ez nem volt tesztelve, lehet, hogy jol mukodik, de olyan tobbszintes album eseten,
+    # ahol az alalbumokat nem egyidoben exportaljuk, szinte biztos, hogy nem jo, ilyenkor a
+    # script hasznalata nem is javasolt
+
+    # tobbszintes album eseten a contentben mappa jon letre, ennek a permissionjeit is frissitjuk
+    mla_content_path="/srv/spotweb/content/photo/${album_path:0:4}/${album_path,,}"
+    # ha letezik ilyen nevu directory
+    if [ -d "$mla_content_path" ] ; then
+        chmod -R 775 ${mla_content_path}
+        chgrp -R users ${mla_content_path}
+    fi
+}
+
+echo "Export inditasa a kovetkezo helyen talalhato albumra: ${drive_name}:/${album_path}"
+
+
+cd /mnt/archive/google_drive_downloads/simon/
+
+# &&-ekkel kotjuk ossze, hogy csak akkor fusson le a kovetkezo, ha az elozo sikeresen lefutott
+# a sleepek csak a biztonsag kedveert szerepelnek, jo esellyel elhagyhatok
+rclone copy ${drive_name}:/${album_path} ./${album_path} -P && sleep 0.5 && 
+check_json && 
+python3 scripts/spot_export_VinceMod.py -i ./${album_path}/ -l ./logs/${album_path}.log && sleep 0.5 && 
+bash scripts/convert.sh ./logs/${album_path,,}.log && sleep 0.2 && 
+cd /srv/spotweb && sleep 0.2 && 
+hugo --noTimes && 
+update_permissions && 
+export_end=`date +%s` && echo "Az album exportalasa $((export_end-export_start)) masodperc alatt befejezodott."
+
+# TODO: tobbszintes albumnal is meg lehetne nezni, pl ha a content/photo/2021-en belul van album_neve nevu mappa, akkor abban rekurzivan at lehetne allitani
+echo "Ne felejtsd el ellenorizni, hogy a jogosultsagok megfeleloen modositva lettek-e az exportalt album fajljain. Tobbszintes album eseten a script ezeket meg sem probalja modositani!"
+printf "\n\n"
+echo "Permissionok:"
+echo "/srv/spotweb/web_images/2021-en belul:"
+ls -la /srv/spotweb/web_images/${album_path:0:4}/ | grep ${album_path,,}
+echo "2048-on belul:"
+ls -la /srv/spotweb/web_images/${album_path:0:4}/${album_path,,}/2048 | tail -n 10
+echo "contenten belul:"
+ls -la /srv/spotweb/content/photo/${album_path:0:4}/ | grep ${album_path,,}
