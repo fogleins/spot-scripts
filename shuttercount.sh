15e4a6d38389a53c509f768081dc189907bcfc21
fogleins <fogleins@hotmail.hu>
2022-05-09 17:30:13 +0200

    Add shuttercount.sh

---
+++ shuttercount.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# egy mappában található minden .NEF fájlon végigmegy, és kiolvassa az exifből
+#   a következő adatokat:
+#       -Fájl létrehozási ideje
+#       -Kamera típusa (modell)
+#       -Sorozatszám (azonos modellek különböző példányainak megkülönböztetéséhez)
+#       -Expószám
+#
+#   Használat: bash ./shuttercount.sh <kepeket-tartalmazo-mappa-eleresi-utja>  # szokozoket nem tartalmazhat!
+#           vagy: cd-zzunk a képeket tartalmazó mappába és bash ./scriptet/tartalmazo/mappa/shuttercount.sh
+#               a képeket tartalmazó mappa (amibe cd-ztünk) ebben az esetben sem tartalmazhat szóközt
+
+directory="$1"
+
+if [ "$directory" == "" ]; then
+    directory=$(pwd)
+# remove the trailing slash if it's given, making the concatenation easier to read
+elif [ "${directory: -1}" == "/" ]; then
+    directory="${directory:0:-1}"
+fi
+
+echo $directory
+
+directory="$directory/*.NEF"
+for file in $directory; do
+    exiftool $file | grep -E 'Serial Number|Shutter Count|File Modification Date/Time|Camera Model Name'
+    echo ""
+done
